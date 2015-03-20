08cde0cf68510c379f06b48f774046da5ea44f9a
Jordan McCoy <mccoy.jordan@gmail.com>
2015-03-20 16:38:01 -0500

    bumped minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scheme',
-    version='2.0',
+    version='2.0.1',
     description='A declarative schema framework.',
     long_description=open('README.rst').read(),
     license='BSD',
