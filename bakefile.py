8e5c5765d2baf88389a8e0e158ed094d5ea4f91a
Jordan McCoy <mccoy.jordan@gmail.com>
2015-03-21 19:30:04 -0500

    bakefile updates

--- bakefile.py
+++ bakefile.py
@@ -1,8 +1,20 @@
 from bake import *
 
 @task()
+def buildpkg(runtime):
+    Path('dist').rmtree(True)
+    runtime.shell('python setup.py sdist bdist_wheel')
+    Path('build').rmtree()
+    Path('scheme.egg-info').rmtree()
+
+@task()
 def buildhtml(runtime):
     runtime.execute('sphinx.html', sourcedir='docs')
+
+@task()
+def clean(runtime):
+    for target in ('build', 'cover', 'dist', 'scheme.egg-info'):
+        Path(target).rmtree(True)
 
 @task()
 def runtests(runtime):
