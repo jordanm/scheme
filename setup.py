2c97105ba68804cf30d211d9552029b0948bf0c0
Defelo <elodef42@gmail.com>
2019-11-15 13:09:10 +0100

    fixed urlparse import

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scheme',
-    version='2.0.2',
+    version='2.0.3',
     description='A declarative schema framework.',
     long_description=open('README.rst').read(),
     license='BSD',
